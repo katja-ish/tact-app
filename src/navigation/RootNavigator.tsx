b0e43c95e2dba7237e56cdc5dc6f8bc886565613
semilunar <katyanikitina.me@yandex.ru>
2020-10-03 22:54:07 +0300

    screens swipe disabled

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -3,6 +3,7 @@ import { FriendList, ConversationsList, Settings } from '@app/screens';
 import { ScreenNames, RootStackParamList } from '@app/types';
 import { enableScreens } from 'react-native-screens';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
 import AnimatedTabBar from './AnimatedTabBar';
 
 const Tab = createMaterialTopTabNavigator<RootStackParamList>();
@@ -11,7 +12,10 @@ enableScreens();
 
 export default () => {
   return (
-    <Tab.Navigator tabBar={(props) => <AnimatedTabBar {...props} />} tabBarPosition="bottom">
+    <Tab.Navigator
+      swipeEnabled={false}
+      tabBar={(props) => <AnimatedTabBar {...props} />}
+      tabBarPosition="bottom">
       <Tab.Screen name={ScreenNames.FRIEND_LIST} component={FriendList} />
       <Tab.Screen name={ScreenNames.CONVERSATIONS_LIST} component={ConversationsList} />
       <Tab.Screen name={ScreenNames.SETTINGS} component={Settings} />
