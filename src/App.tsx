52ffd90e70eac0ba74a1053238f1cf2cc6d4d8ad
semilunar <katyanikitina.me@yandex.ru>
2021-02-19 15:51:42 +0300

    firebase auth start

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-
 import RootNavigator from './navigation/RootNavigator';
 import store from './store';
+import auth from '@react-native-firebase/auth';
+import { Text } from './theme';
+import { View } from 'react-native';
 
 const App = () => {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+
   return (
     <Provider store={store}>
       <NavigationContainer>
